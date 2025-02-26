43a76e52949a6298f0df8007c4f228c745c6cc6a
zouxiaochuan <zouxiaochuan@163.com>
2025-02-26 11:22:40 +0800

    first commit

---
+++ face_apis/face_api.py
@@ -0,0 +1,10 @@
+
+class FaceApi(object):
+    def __init__(self, model_folder):
+        pass
+
+    def build_index(self, ids, images):
+        pass
+
+    def detect_and_search(self, image):
+        pass
